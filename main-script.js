af4211e023fae3ea46c379211e0fd9ff54f1e379
Darya Gluschenko <darya@gluschenko.by>
2023-08-30 01:44:54 +0200

    add sending emails

--- main-script.js
+++ main-script.js
@@ -12,6 +12,29 @@ function isIOS() {
   return /iPhone|iPad|iPod/i.test(navigator.userAgent);
 }
 
+function storeEmail(email) {
+  const url = "https://aj4vwojhub.execute-api.ap-southeast-2.amazonaws.com/landing-api"; // Replace with your API endpoint
+  const data = {
+    email: email
+  };
+
+fetch(url, {
+  method: "POST",
+  mode: 'no-cors',
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify(data)
+})
+  .then(response => response.json())
+  .then(data => {
+    console.log("Response data:", data);
+  })
+  .catch(error => {
+    console.error("Error:", error);
+  });
+}
+
   
 // hide app store and play market buttons for laptop
 const storeButtons = document.querySelectorAll('.article-content-button');
@@ -60,7 +83,7 @@ const submitEmailForm = document.getElementById("email-subscription-form");
 submitEmailForm.addEventListener("submit", function(event) {
   event.preventDefault();
 
-  const emailInput = document.getElementById("email")
-  console.log("Email on submit:", emailInput.value);
+  const emailInput = document.getElementById("email");
+  storeEmail(emailInput.value);
 });
 
